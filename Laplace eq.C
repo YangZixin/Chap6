5fdb315a57854908511812e59e84ed1807c9bcc8
YangZixin <zixinyangcn@gmail.com>
2013-05-27 19:23:47 +0800

    Now what?

--- Laplace eq.C
+++ Laplace eq.C
@@ -4,7 +4,7 @@
 #include <fstream>
 using namespace std;
 
-const int LengthOfCapacitor = 10;
+const int LengthOfCapacitor = 1;
 const double GridInterval = 0.1;
 const int GridCheck = LengthOfCapacitor * 10;
 double potential[GridCheck][GridCheck];
@@ -28,6 +28,7 @@ void update_V()
         {
             temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
             delta_V += sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
+    cout<<delta_V<<endl;
         }
     }
     for (int i = 1; i < GridCheck-1; i++)
@@ -45,7 +46,7 @@ int laplace_calculate()
     double PermitErrorV = GridCheck * GridCheck * pow(10, -5);
     do
     {
-        update_V;
+        update_V();
         N_iter++;
     }
     while(delta_V > PermitErrorV);
@@ -57,5 +58,14 @@ int main()
 	initializa_V();
     int N_iter = laplace_calculate();
     cout<<N_iter<<endl;
+    for (int i = 0; i < GridCheck; ++i)
+    {
+        for (int j = 0; j < GridCheck; ++j)
+        {
+            cout<<potential[i][j]<<"    ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
