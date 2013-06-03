26e432cd78d0f09259355096d2a2147b85b8aab7
YangZixin <zixinyangcn@gmail.com>
2013-06-03 18:25:00 +0800

    ROOT TGraph .dat file

--- IdealWave.C
+++ IdealWave.C
@@ -5,16 +5,16 @@
 using namespace std;
 
 const double r_stepsize = 1;
-const int StringLength = 100;
+const int StringLength = 1000;
 const double initPosition = 0.2;
-const double k = 0.1;
-const int TimeStep = 10;
-double y_Amp[StringLength][TimeStep];
+const double k = 0.001;
+const int TimeStep = 975;
+double y_Amp[StringLength + 1][TimeStep];
 
 void initString()
 {
 	int x0 = StringLength * initPosition;
-	for (int i = 1; i < StringLength - 1; ++i)
+	for (int i = 1; i < StringLength; ++i)
 	{
 		y_Amp[i][0] = exp(-k * (i - x0) * (i - x0));
 		y_Amp[i][1] = exp(-k * (i - x0) * (i - x0));
@@ -24,8 +24,8 @@ void initString()
 void propagate(int Times)
 {
 	y_Amp[0][Times] = 0;
-	y_Amp[StringLength - 1][Times] = 0;
-	for (int i = 1; i < StringLength - 1; ++i)
+	y_Amp[StringLength][Times] = 0;
+	for (int i = 1; i < StringLength; ++i)
 	{
 		y_Amp[i][Times] = 2 * (1 - r_stepsize*r_stepsize) * y_Amp[i][Times - 1] - y_Amp[i][Times - 2] + r_stepsize * r_stepsize * (y_Amp[i + 1][Times - 1] + y_Amp[i - 1][Times - 1]);
 	}
@@ -35,16 +35,17 @@ void propagate(int Times)
 int main()
 {
 	initString();
-	int TimeT = 50;
-	for (int j = 2; j < TimeT; ++j)
+	for (int j = 2; j < TimeStep; ++j)
 	{
 		propagate(j);
 	}
-	for (int i = 0; i < StringLength; ++i)
+	for (int i = 0; i <= StringLength; ++i)
     {
-        cout<<y_Amp[i][TimeT -1]<<"    "<<endl;
+    	double Len = i * 0.001;
+        cout<<Len<<"    "<<y_Amp[i][TimeStep -1]<<endl;
     }
     cout<<endl;
     return 0;
 
 }
+
